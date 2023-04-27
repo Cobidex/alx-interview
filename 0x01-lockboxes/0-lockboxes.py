86b41a1d1abb57b22cb1f4ea6c2366be022176cd
Stephen Obialor <stevobialor@gmail.com>
2023-04-27 13:18:11 +0000

    checks if boxes can be opened

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''contains the unlockall method'''
+
+def canUnlockAll(boxes):
+    '''determines if all the boxes can be opened'''
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    queue = [0]
+    while queue:
+        box = queue.pop(0)
+        for key in boxes[box]:
+            if key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+    return all(visited)
