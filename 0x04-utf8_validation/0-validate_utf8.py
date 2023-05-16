ef6d1a68253702c48c766d0c5c3bc166ae01e19c
Stephen Obialor <stevobialor@gmail.com>
2023-05-16 06:12:46 +0000

    validate data to know if utf-8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+This module contains the validUTF8 method
+'''
+
+
+def validUTF8(data):
+    '''
+    determines if a given data set represents a valid UTF-8 encoding
+    '''
+    cont_bytes: int = 0
+    store_init_cont_bytes: int = cont_bytes
+    for byte in data:
+        if cont_bytes:
+            if byte >> 6 != 0b10:
+                return False
+            cont_bytes -= 1
+        else:
+            if byte >> 7 == 0b0:
+                pass
+            elif byte >> 5 == 0b110:
+                cont_bytes = 1
+            elif byte >> 4 == 0b1110:
+                cont_bytes = 2
+            elif byte >> 3 == 0b11110:
+                cont_bytes = 3
+            else:
+                return False
+
+    if cont_bytes:
+        return False
+    return True
