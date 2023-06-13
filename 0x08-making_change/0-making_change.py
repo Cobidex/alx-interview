33ec2b6678e91c90ac6fd89125eed2ba3c8c05d9
Stephen Obialor <stevobialor@gmail.com>
2023-06-13 12:20:12 +0000

    determine the fewest number of coins needed to meet a given amount

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" contains the makeChange function """
+from typing import List, Dict
+
+
+def recurseMakeChange(coins: List[int], utils: Dict) -> int:
+    """ recursively determine fewest number of coins """
+    if utils['index'] >= utils['length'] and utils['remainder'] > 0:
+        return -1
+    elif utils['index'] == utils['length']:
+        return utils['numberCoins']
+    elif coins[utils['index']] > utils['remainder']:
+        utils['index'] += 1
+        return recurseMakeChange(coins, utils)
+    else:
+        utils['numberCoins'] += utils['remainder'] // coins[utils['index']]
+        utils['remainder'] = utils['remainder'] % coins[utils['index']]
+        return recurseMakeChange(coins, utils)
+
+
+def makeChange(coins: List[int], total: int) -> int:
+    """determine fewest number of coin
+    needed to meet a given amount
+    """
+    if total <= 0:
+        return 0
+
+    index: int = 0
+    length: int = len(coins)
+    coins.sort(reverse=True)
+    utils: Dict = {
+            'remainder': total,
+            'length': length,
+            'index': 0,
+            'numberCoins': 0
+            }
+
+    return recurseMakeChange(coins, utils)
