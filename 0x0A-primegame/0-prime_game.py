e3de1f8f02f3efeb0f3927370e6619b768c1e55c
Stephen Obialor <stevobialor@gmail.com>
2023-06-29 19:53:46 +0000

    done

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
 """
-contains the isWinner function
+contains the prime game
 """
 
 
 def isWinner(num_rounds, nums):
-    """function for the game"""
+    """performs prime game"""
     if not nums or num_rounds < 1:
         return None
+    
     max_num = max(nums)
     prime_filter = [True for _ in range(max(max_num + 1, 2))]
-    for i in range(2, int(pow(max_num, 0.5)) + 1):
-        if not prime_filter[i]:
-            continue
-        for j in range(i * i, max_num + 1, i):
-            prime_filter[j] = False
     prime_filter[0] = prime_filter[1] = False
-    primes_count = 0
+
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
+        if prime_filter[i]:
+            for j in range(i * i, max_num + 1, i):
+                prime_filter[j] = False
+    
+    primes_count = [0] * len(prime_filter)
+    count = 0
     for i in range(len(prime_filter)):
         if prime_filter[i]:
-            primes_count += 1
-        prime_filter[i] = primes_count
-    player1_count = 0
-    for k in nums:
-        player1_count += prime_filter[k] % 2 == 1
+            count += 1
+        primes_count[i] = count
+
+    player1_count = sum(primes_count[n] % 2 == 1 for n in nums)
+
     if player1_count * 2 == len(nums):
         return None
     if player1_count * 2 > len(nums):
